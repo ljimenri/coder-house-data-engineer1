e3938a9e2adadd91ea51d47158f659161b0cbc67
Luis <luisjimenez@MacBook-Pro-de-Luis.local>
2024-08-01 20:56:16 -0400

    add my favorite artists

--- src/ApiConnector/ApiConnector.py
+++ src/ApiConnector/ApiConnector.py
@@ -3,23 +3,21 @@ from dotenv import load_dotenv
 import os
 import json
 def getToken():
-    
+    # Carga de variables de entorno
     load_dotenv()
-    #Definicion de variables de entorno
+    # Definicion de variables de entorno
     url = "https://accounts.spotify.com/api/token"
-
     # Encabezados de la solicitud
     headers = {
         "Content-Type": "application/x-www-form-urlencoded"
     }
-
     # Datos del cuerpo de la solicitud
     data = {
         "grant_type": "client_credentials",
         "client_id": os.getenv('CLIENT_ID'),
         "client_secret": os.getenv('CLIENT_SECRET')
     }
-
+    # Configuración de respuesta
     response = requests.post(url, headers=headers, data=data)
     if response.status_code == 200:
         token = response.json().get('access_token')
@@ -28,11 +26,13 @@ def getToken():
         print(f"Error en la Api: no se ha podido obtener token")
 
 def callApi(token):
+    # Datos del cuerpo de la solicitud
     url="https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb"
     header_value="Bearer"+" "+token
     headers = {
     "Authorization": header_value
     }
+    # Configuración de respuesta
     response=requests.get(url, headers=headers)
     if response.status_code == 200:
         return_api = response.json()
